832ad0b16d95f1b42d859c8e4d44e454067725aa
Vicentesan <vikom.sanchez@gmail.com>
2025-04-03 13:15:01 -0300

    refac: use rectangle to call perimeter

--- structures/shapes.go
+++ structures/shapes.go
@@ -15,8 +15,8 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
 type Circle struct {
