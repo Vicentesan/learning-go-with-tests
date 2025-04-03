bccc2369d139ceb3087ab64ff6a1af4af8de4990
Vicentesan <vikom.sanchez@gmail.com>
2025-04-03 08:08:35 -0300

    feat: add repeat

---
+++ iteration/repeat.go
@@ -0,0 +1,19 @@
+package iteration
+
+import "strings"
+
+const defaultRepeatCount = 5
+
+func Repeat(character string, timesArg ...int) string {
+	var repeated strings.Builder
+	
+	repeatCount := defaultRepeatCount
+	if len(timesArg) > 0 {
+		repeatCount = timesArg[0]
+	}
+
+	for i := 0; i < repeatCount; i++ {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
